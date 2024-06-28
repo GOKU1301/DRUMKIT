da0ed4704d17d9d066b32a3c3cdba559070980f6
GOKU1301 <154021053+GOKU1301@users.noreply.github.com>
2024-06-28 19:12:27 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,60 @@
+var buttons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < buttons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    sounds(this.innerHTML);
+    addanimation(this.innerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  sounds(event.key);
+  addanimation(event.key);
+});
+
+document.addEventListener("touchstart", function(event) {
+  if (!event.target.classList.contains("drum")) {
+    alert("Wrong button pressed:(");
+  }
+});
+
+function sounds(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("./sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+    default:
+      alert("Wrong button pressed.:(");
+  }
+}
+
+function addanimation(key) {
+  document.querySelector("." + key).classList.add("pressed");
+  setTimeout(function() {
+    document.querySelector("." + key).classList.remove("pressed")
+  }, 100);
+}
